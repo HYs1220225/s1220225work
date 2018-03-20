fdcfdfcf4f4f2d58ccb3973e5cfd3ea04356c7a2
Hokuto Yamaguchi <s1220225@Hokuto-no-Mac-mini.local>
2018-03-20 12:14:06 +0900

    add new file

--- Fmechanical.cpp
+++ Fmechanical.cpp
@@ -6,7 +6,7 @@
 #include <openvdb/tools/VolumeToMesh.h>
 #include <openvdb/tools/LevelSetRebuild.h>
 #include <openvdb/tools/LevelSetFilter.h>
-#include <openvdb/util/CpuTimer.h>
+// #include <openvdb/util/CpuTimer.h>
 // #include <vector>
 #include "Fmechanical.h"
 #include "mechanical.h"
@@ -20,22 +20,22 @@ using namespace openvdb;
 void Fmecha::create(FloatGrid::Ptr grid, const CoordBBox& indexBB, float h){
   typename FloatGrid::Accessor accessor = grid->getAccessor();
   float distance;
-  cout << "run mechanical" << endl;
+  cout << "run sphere" << endl;
   
   for (Int32 i = indexBB.min().x(); i <= indexBB.max().x(); ++i) {
     for (Int32 j = indexBB.min().y(); j <= indexBB.max().y(); ++j) {
       for (Int32 k = indexBB.min().z(); k <= indexBB.max().z(); ++k) {
-	// transform point (i,j,k) of index space into world space
-	// assume that hx=hy=hz=h (each cell is a cube)
-	Vec3f p(i*h, j*h, k*h);
+ 	// transform point (i,j,k) of index space into world space
+ 	// assume that hx=hy=hz=h (each cell is a cube)
+ 	Vec3f p(i*h, j*h, k*h);
 
-	// compute level set function value
-	// note the convention: negative inside / positive outside
+ 	// compute level set function value
+ 	// note the convention: negative inside / positive outside
 
-	distance = -eval(p.x(), p.y(), p.z());
+ 	distance = -eval(p.x(), p.y(), p.z());
 
-	// set the value in the grid
-	accessor.setValue(Coord(i,j,k), distance);
+ 	// set the value in the grid
+ 	accessor.setValue(Coord(i,j,k), distance);
       }
     }
   }
